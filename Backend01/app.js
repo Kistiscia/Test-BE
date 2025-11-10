903f3899fb3c1b9b1036a99d3dc16029180a7f8e
Mixky <sirawatma4@gmail.com>
2025-11-10 23:10:47 +0700

    VM

--- Backend01/app.js
+++ Backend01/app.js
@@ -21,3 +21,9 @@ app.use('/', indexRouter);
 
 
 module.exports = app;
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${port}`);
+});
--- Backend01/repositories/classroomRepository.js
+++ Backend01/repositories/classroomRepository.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
     host: "localhost",
-    user: "root",
+    user: "mix",
     password: "5189",
     database: "mydb",
 });
